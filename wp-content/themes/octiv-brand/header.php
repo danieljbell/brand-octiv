d20c005423924bc98ac5ab009597c87d8c1dfc6a
danieljbell <djbell70@gmail.com>
2017-03-07 17:06:11 -0500

    initialize repo

---
+++ wp-content/themes/octiv-brand/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <base href="<?php echo site_url(); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title><?php wp_title(''); ?></title>
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,700' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <?php wp_head(); ?>
+
+</head>
+<body <?php body_class(); ?>>
+
+<header>
+  <div class="site-width">
+    <div class="half">
+      <div>
+        <a href="/" title="Octiv Design System Homepage" class="site-logo">Logo</a>
+      </div>
+      <div>
+        <ul>
+          <li><a href="#0">Link</a></li>
+          <li><a href="#0">Link</a></li>
+          <li><a href="#0">Link</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</header>
---
+++ wp-content/themes/octiv-brand/index.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+  <h1>Daniel</h1>
+<?php get_footer(); ?>
