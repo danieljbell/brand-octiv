c4606f3cbeae67a1095b8f4bbc2cf5339da8be61
danieljbell <djbell70@gmail.com>
2017-03-16 16:46:04 -0400

    thinking grid is done

--- wp-content/themes/octiv-brand/footer.php
+++ wp-content/themes/octiv-brand/footer.php
@@ -1,3 +1,8 @@
 <?php wp_footer(); ?>
+<?php if (is_home()) : ?>
+  <script type="text/javascript" src="<?php echo get_stylesheet_directory_URI(); ?>/src/js/_prism.js">
+
+  </script>
+<?php endif; ?>
 </body>
 </html>
