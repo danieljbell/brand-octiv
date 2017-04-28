96b9e6f6c0d83394a45ecd2d2c4884f84a4c0bc9
danieljbell <djbell70@gmail.com>
2017-04-28 11:03:26 -0400

    cleaning up forms and home page

--- wp-content/themes/octiv-brand/index.php
+++ wp-content/themes/octiv-brand/index.php
@@ -15,68 +15,6 @@
   </div>
 </section>
 
-<section>
-  <div class="site-width">
-    <script src="//app-sj20.marketo.com/js/forms2/js/forms2.min.js"></script>
-    <form id="mktoForm_1008"></form>
-    <script>
-      MktoForms2.loadForm("//app-sj20.marketo.com", "625-MXY-689", 1008, function(form) {
-        var selectBoxes = form.getFormElem()[0].querySelectorAll('form select');
-        for (var i = 0; i < selectBoxes.length; i++) {
-          selectBoxes[i].classList.add('fancy');
-        }
-        var newsletterBox = document.querySelector('label[for="subscriptionNewsletter"]');
-        newsletterBox.parentElement.classList.add('mktoFlexWrap');
-        newsletterBox.querySelector('.mktoAsterix').remove();
-
-        // Blacklisted Email Domains
-        var invalidDomains = ["@gmail.","@yahoo.","@hotmail.","@live.","@aol.","@outlook."];
-
-        //Add an onValidate handler
-        form.onValidate(function(values, followUpUrl) {
-
-          // Verify Email is Business Domain
-          var email = form.vals().Email;
-          if(email){
-            if(!isEmailGood(email)) {
-              form.submitable(false);
-              var emailElem = form.getFormElem().find("#Email");
-              form.showErrorMessage("Must be Business email.", emailElem);
-            } else{
-              form.submitable(true);
-            }
-          }
-
-        function isEmailGood(email) {
-          for(var i=0; i < invalidDomains.length; i++) {
-            var domain = invalidDomains[i];
-            if (email.indexOf(domain) != -1) {
-              return false;
-            }
-          }
-          return true;
-        }
-
-        });
-
-        form.onSuccess(function(values, followUpUrl) {
-          // Get the form field values
-          var vals = form.vals();
-
-          // Update the redirect url with form fields
-          followUpUrl = window.location.origin + '/thank-you/?first_name=' + vals.FirstName;
-
-          // Redirect the page with form field
-          location.href = followUpUrl;
-
-          // Return false to prevent the submission handler continuing with its own processing
-          return false;
-        });
-      });
-    </script>
-  </div>
-</section>
-
 <?php
   if (is_search()) :
     if (have_posts()) :
